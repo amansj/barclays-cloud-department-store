eb396c7c5e97b361221ef6a513691b9da20c49b9
Aman Kumar Singh <apkaman9@gmail.com>
2022-07-03 13:23:51 +0530

    payment check

--- src/main/java/com/barclays/store/BarclaysCloudDepartmentStoreApplication.java
+++ src/main/java/com/barclays/store/BarclaysCloudDepartmentStoreApplication.java
@@ -1,12 +1,13 @@
 package com.barclays.store;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class BarclaysCloudDepartmentStoreApplication {
+public class BarclaysCloudDepartmentStoreApplication implements CommandLineRunner{
 	
 	@Bean
 	public RestTemplate getRestTemplate() {
@@ -15,6 +16,12 @@ public class BarclaysCloudDepartmentStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarclaysCloudDepartmentStoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
 	}
 
 }
--- src/main/java/com/barclays/store/config/WebSecurityConfig.java
+++ src/main/java/com/barclays/store/config/WebSecurityConfig.java
@@ -81,7 +81,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			}
 		}).and().csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/user/signup","/user/token","/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll().
+				.authorizeRequests().antMatchers("/user/signup","/user/token","/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**","/payment/accept/notification/**").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
